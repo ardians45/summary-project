3f2bcbd031d40a23a28f6d5ea02f9ae1704d8990
Ardian Setiawan <ardians1945@gmail.com>
2024-06-14 23:32:44 +0700

    update

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-// Contoh fungsi untuk menampilkan alert
-function showAlert() {
-  alert("Website ini masih dalam pengembangan!");
-}
+window.onload = function () {
+  const img = document.querySelector(".profile-picture");
+  img.classList.add("loaded");
+};
+
+document.querySelectorAll(".nav-item").forEach(function (item) {
+  item.addEventListener("click", function () {
+    var alertContainer = document.getElementById("alert-container");
+    var alert = document.createElement("div");
+    alert.className = "alert alert-warning alert-dismissible fade show";
+    alert.role = "alert";
+    alert.innerHTML = `
+        Website masih dalam tahap pengembangan.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      `;
+    alertContainer.appendChild(alert);
+  });
+});
